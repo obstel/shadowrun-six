448dae7228157bf7af19a64e349cea1b3d786d43
Christian Oberstaedt <christian.oberstaedt@googlemail.com>
2020-10-26 23:38:34 +0100

    Item-Sheet Test

--- modules/sheets/SR6_ItemSheet.js
+++ modules/sheets/SR6_ItemSheet.js
@@ -1,5 +1,5 @@
 export default class SR6_ItemSheet extends ItemSheet {
     get template() {
-        return 'systems/shadowrun-six/templates/sheets/{$this.item.data.type}-sheet.html';
+        return 'systems/shadowrun-six/templates/sheets/weapon-sheet.html';
     }
 }
--- shadowrun-six.js
+++ shadowrun-six.js
@@ -3,6 +3,6 @@ import SR6_ItemSheet from "./modules/sheets/SR6_ItemSheet.js";
 Hooks.once("init", function () {
     console.log("SR6 | Initialising Shadowrun 6th System");
 
-    Items.unregister("core", ItemSheet);
+    Items.unregisterSheet("core", ItemSheet);
     Items.registerSheet("shadowrun-six", SR6_ItemSheet, { makeDefault: true });
 });
