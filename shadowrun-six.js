cf2b98637fde11c81f29ba2802bc4aee614e4372
Christian Oberstaedt <christian.oberstaedt@googlemail.com>
2020-10-26 23:26:33 +0100

    Item-Sheet Test

--- shadowrun-six.js
+++ shadowrun-six.js
@@ -1,8 +1,8 @@
-import SR6_ItemSheet from "./modules/sheets/SR6_ItemSheet";
+import SR6_ItemSheet from "./modules/sheets/SR6_ItemSheet.js";
 
 Hooks.once("init", function () {
     console.log("SR6 | Initialising Shadowrun 6th System");
 
     Items.unregister("core", ItemSheet);
-    Items.registerSheet("shadowrun-six", SR6_ItemSheet, { makeDefault: true});
-})
+    Items.registerSheet("shadowrun-six", SR6_ItemSheet, { makeDefault: true });
+});
